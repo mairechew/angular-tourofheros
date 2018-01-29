2b4741b96e4704abe04fe66c4722b930f79d56ca
Maire Chew <mairechew@gmail.com>
2018-01-29 14:22:46 -0700

    building file structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroComponent } from './hero/hero.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    HeroComponent
   ],
   imports: [
     BrowserModule
